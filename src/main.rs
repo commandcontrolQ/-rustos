004bc34b60aba985da25378335e6cedc8023ecd1
CtrlAltDel <boyrazhalil@outlook.com>
2023-02-07 13:31:01 +0000

    Partial update to 'Testing' section

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-// main.rs
-
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 use core::panic::PanicInfo;
 
@@ -11,6 +20,9 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -19,4 +31,11 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
